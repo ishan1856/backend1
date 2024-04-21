d6e2d77e5b908b6f672df41da1f49fbb33a88b73
Ishan Singh <108198105+ishan1856@users.noreply.github.com>
2024-04-21 07:33:04 +0530

    github data edit

--- index.js
+++ index.js
@@ -5,8 +5,17 @@ const dotenv=require('dotenv');
 dotenv.config();
 
 const port=process.env.PORT;
+
+const githubdata={
+    "name":"Ishan",
+    "age":21,
+    "role":'developer'
+}
 app.get('/', (req, res) => {
   res.send('Hello Worlld!')
+})
+app.get('/github', (req, res) => {
+  res.json(githubdata)
 })
 
 app.listen(port, () => {
